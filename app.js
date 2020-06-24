38cf33a7e8b624d6863302169c502c2ebca7fe6a
Stepan Popov <bimimot@yandex.ru>
2020-06-24 14:41:05 +0300

    Did GET-require for users with promise

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-const path = require("path");
-app.use(express.static(path.join(__dirname, 'public'))); // доступ только к публичным файлам
+const path = require("path");                             //модуль ноды для работы с путями файлов
+const fs = require("fs");                                 //модуль ноды для работы с файлами
+const fsPromises = require('fs').promises;                //модуль ноды для работы с промисами
 
-app.listen(PORT);
+
+
+
+app.use(express.static(path.join(__dirname, 'public'))); // доступ только к публичным файлам
+app.listen(PORT);                                        //начинаем слушать заданный порт
 
 app.get('/users', (req, res) => {
-    res.send(animals[req.query.animal][req.query.type]);
+  fsPromises.readFile('./data/users.json', { encoding: 'utf8' })      //смотрим промис чтения файла
+      .then((data) => {                                               //если успешно - возвращаем данные
+          res.send(JSON.parse(data));
+      })
+      .catch(err => {                                                 //при ошибке - возвращаем ошибку
+          res.send(err);
+      });
 });
