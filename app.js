2957eb49d444a50fc2c4d871f824c9cda51482e8
Stepan Popov <bimimot@yandex.ru>
2020-06-24 15:12:27 +0300

    Added GET-require for cards with promise

--- app.js
+++ app.js
@@ -9,17 +9,25 @@ const fsPromises = require('fs').promises;                //модуль нод�
 
 
 
-const rootPath =
+const usersPath = path.join(__dirname, 'data', 'users.json');     //собрали абсолютный путь к файлу users.json
+const cardsPath = path.join(__dirname, 'data', 'cards.json');     //собрали абсолютный путь к файлу cards.json
+const publicPath = path.join(__dirname, 'public');                //собрали абсолютный путь к папке public
 
-app.use(express.static(path.join(__dirname, 'public'))); // доступ только к публичным файлам
-app.listen(PORT);                                        //начинаем слушать заданный порт
-
-
-const usersPath = path.join(__dirname, 'data', 'users.json');
-
+app.use(express.static(publicPath));                             // доступ только к публичным файлам
+app.listen(PORT);                                                //начинаем слушать заданный порт
 
 app.get('/users', (req, res) => {
-  fsPromises.readFile(usersPath, { encoding: 'utf8' })      //смотрим промис чтения файла
+  fsPromises.readFile(usersPath, { encoding: 'utf8' })                //смотрим промис чтения файла
+      .then((data) => {                                               //если успешно - возвращаем данные
+          res.send(JSON.parse(data));
+      })
+      .catch(err => {                                                 //при ошибке - возвращаем ошибку
+          res.send(err);
+      });
+});
+
+app.get('/cards', (req, res) => {
+  fsPromises.readFile(cardsPath, { encoding: 'utf8' })                //смотрим промис чтения файла
       .then((data) => {                                               //если успешно - возвращаем данные
           res.send(JSON.parse(data));
       })
