6d676c344dcc678b214d646f0c65500bdf0c25f4
Stepan Popov <bimimot@yandex.ru>
2020-06-24 15:51:57 +0300

    Added get for user/id with promise

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ const publicPath = path.join(__dirname, 'public');                //собрал
 app.use(express.static(publicPath));                             // доступ только к публичным файлам
 app.listen(PORT);                                                //начинаем слушать заданный порт
 
+app.get('/cards', (req, res) => {
+  fsPromises.readFile(cardsPath, { encoding: 'utf8' })                //смотрим промис чтения файла
+      .then((data) => {                                               //если успешно - возвращаем данные
+          res.send(JSON.parse(data));
+      })
+      .catch(err => {                                                 //при ошибке - возвращаем ошибку
+          res.send(err);
+      });
+});
+
 app.get('/users', (req, res) => {
   fsPromises.readFile(usersPath, { encoding: 'utf8' })                //смотрим промис чтения файла
       .then((data) => {                                               //если успешно - возвращаем данные
@@ -26,12 +36,37 @@ app.get('/users', (req, res) => {
       });
 });
 
-app.get('/cards', (req, res) => {
-  fsPromises.readFile(cardsPath, { encoding: 'utf8' })                //смотрим промис чтения файла
-      .then((data) => {                                               //если успешно - возвращаем данные
-          res.send(JSON.parse(data));
-      })
-      .catch(err => {                                                 //при ошибке - возвращаем ошибку
-          res.send(err);
-      });
+app.get('/users/:id', (req, res) => {
+  const{id} = req.params;                                         //запоминаем запрошенный id
+  fsPromises.readFile(usersPath, { encoding: 'utf8' })                //смотрим промис чтения файла
+  .then((data) => {                                               //если успешно - возвращаем данные
+      const usersArr = JSON.parse(data);                          //собираем массив с данными пользователей
+      for (i =0; i < usersArr.length; i++){                                // проходим по всему массиву
+        if (usersArr[i]._id === id) {                          //если находим юзера с нужным id - выводим его данным
+          res.send(usersArr[i]);
+          return
+        }
+      };
+  })
+  .catch(err => {                                                 //при ошибке - возвращаем ошибку
+      res.send(err);
+  });
 });
+
+// function findUser(userId) {
+//   fsPromises.readFile(usersPath, { encoding: 'utf8' })                //смотрим промис чтения файла
+//   .then((data) => {                                               //если успешно - возвращаем данные
+//       const usersArr = JSON.parse(data);                          //собираем массив с данными пользователей
+//       for (i =0; i < usersArr.length; i++){                                // проходим по всему массиву
+//         if (usersArr[i]._id === userId) {                          //если находим юзера с нужным id - выводим его данным
+//           console.log(usersArr[i]);
+//           return
+//         }
+//       };
+//   })
+//   .catch(err => {                                                 //при ошибке - возвращаем ошибку
+//       console.log(err);
+//   });
+// };
+
+// findUser('dbfe53c3c4d568240378b0c6');
