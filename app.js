39537b652fdf9864f0c5e897a6d37c945d250c5b
Stepan Popov <bimimot@yandex.ru>
2020-06-25 20:32:36 +0300

    Fixed syntax problems in app.js

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const path = require('path'); // модуль ноды для работы с путями файлов
 
-const cardsRouter = require(path.join(__dirname, 'routes', 'cards.js')); // импортируем роутер для карточек
-const usersRouter = require(path.join(__dirname, 'routes', 'users.js')); // импортируем роутер для данных о пользователях
-const nolinkRouter = require(path.join(__dirname, 'routes', 'nolink.js')); // импортируем роутер для неверных запросов
+// const cardsPath = path.join(__dirname, 'routes', 'cards.js');
+// const usersPath = path.join(__dirname, 'routes', 'users.js');
+// const nolinkPath = path.join(__dirname, 'routes', 'nolink.js');
+
+const cardsRouter = require('./routes/cards.js'); // импортируем роутер для карточек
+const usersRouter = require('./routes/users.js'); // импортируем роутер для данных о пользователях
+const nolinkRouter = require('./routes/nolink.js'); // импортируем роутер для неверных запросов
 
 const publicPath = path.join(__dirname, 'public'); // собрали абсолютный путь к папке public
 
