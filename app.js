5f9a4cc3763fa061cd58ffdc87c298c137c70cc0
Stepan Popov <bimimot@yandex.ru>
2020-06-27 16:42:34 +0300

    Connected to mongodb: mestodb

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
 
-const app = express();
 const path = require('path'); // модуль ноды для работы с путями файлов
 
+const app = express();
+const { PORT = 3000 } = process.env;
 // const cardsPath = path.join(__dirname, 'routes', 'cards.js');
 // const usersPath = path.join(__dirname, 'routes', 'users.js');
 // const nolinkPath = path.join(__dirname, 'routes', 'nolink.js');
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const cardsRouter = require('./routes/cards.js'); // импортируем роутер для карточек
 const usersRouter = require('./routes/users.js'); // импортируем роутер для данных о пользователях
