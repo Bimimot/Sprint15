6f9a910cc9c175464f3cffaaef0540ad495b3109
Stepan Popov <bimimot@yandex.ru>
2020-06-28 14:04:50 +0300

    Updated regex for links

--- models/card.js
+++ models/card.js
@@ -9,6 +9,13 @@ const cardSchema = new mongoose.Schema({
   },
   link: {
     type: String,
+    validate: {
+      validator(text) {
+        return /^(https?:\/\/)(((www.)?([\w\-]+(\.))+[a-z]{2,})|((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])){3}))(:\d{2,5})?(\/.?[\w\-]+)*#?\/?((\..{2,})?)$/.test(text);
+      },
+      message: (props) => `${props.value} Это неправильная ссылка`
+    },
+
     required: true,
   },
   owner: {
