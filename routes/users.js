a6a985428ffa5427a74d0655a2002cd1d0375251
Stepan Popov <bimimot@yandex.ru>
2020-07-15 23:02:46 +0700

    Added routes: signin & signup

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const {
-  getUsers, getUserById, createUser, patchUser, patchUserAvatar,
+  getUsers, getUserById, createUser, patchUser, patchUserAvatar, login
 } = require('../controllers/users'); // импорт методов из контроллера
 
-router.get('/', getUsers); // вызываем метод
-router.get('/:id', getUserById); // вызываем метод
-router.post('/', createUser); // вызываем метод
-router.patch('/me', patchUser); // вызываем метод
-router.patch('/me/avatar', patchUserAvatar); // вызываем метод
+router.get('/', getUsers); // вызываем метод получения всех пользователей
+router.get('/:id', getUserById); // вызываем метод получения пользователя по id
+router.patch('/me', patchUser); // вызываем метод обновления имени пользователя
+router.patch('/me/avatar', patchUserAvatar); // вызываем метод обновления ссылки на аватар пользователя
+
+router.post('/signin', login); // вызываем метод аторизации пользователя
+router.post('/signup', createUser); // вызываем метод регистрации пользователя
 
 module.exports = router;
