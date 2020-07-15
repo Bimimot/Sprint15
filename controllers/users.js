ce8b91cd7870d4f788c84fb1c7d3bff165e840e2
Stepan Popov <bimimot@yandex.ru>
2020-07-15 22:31:58 +0700

    Added simple login-controller without token

--- controllers/users.js
+++ controllers/users.js
@@ -86,3 +86,30 @@ module.exports.patchUserAvatar = (req, res) => {
       }
     });
 };
+
+// авторизация пользователя
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Неправильные почта или пароль')); // хеши не совпали — отклоняем промис
+      }
+      res.send({ message: 'Всё верно!' });
+      return (true); // return добавлен для соблюдения стандарта eslint
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+};
