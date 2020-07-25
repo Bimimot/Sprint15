4ef712b84a5a9087881985d8921868f622e8e3a4
Stepan Popov <bimimot@yandex.ru>
2020-07-25 18:30:00 +0700

    Installed joi

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate'); // подключаем библиотеку для валидации запросов
 const auth = require('../middlewares/auth'); // подключаем мидлвэру авторизации
 
 const {
@@ -8,8 +9,22 @@ const {
 router.use(auth); // вызываем авторизацию для всех методов идущих ниже
 
 router.get('/', getUsers); // вызываем метод получения всех пользователей
-router.get('/:id', getUserById); // вызываем метод получения пользователя по id
-router.patch('/me', patchUser); // вызываем метод обновления имени пользователя
-router.patch('/me/avatar', patchUserAvatar); // вызываем метод обновления ссылки на аватар пользователя
+
+router.get('/:id', celebrate({
+  params: Joi.object().keys({ id: Joi.string().alphanum().length(24) }),
+}), getUserById); // вызываем метод получения пользователя по id
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), patchUser); // вызываем метод обновления имени пользователя
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().url(),
+  }),
+}), patchUserAvatar); // вызываем метод обновления ссылки на аватар пользователя
 
 module.exports = router;
